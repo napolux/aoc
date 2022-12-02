44759558e8cf112ef47d75202283094f58a87c48
Napolux <napolux@gmail.com>
2022-12-02 18:41:49 +0100

    better comments

--- 2022/day-2/index.ts
+++ 2022/day-2/index.ts
@@ -3,12 +3,12 @@ import { input } from './resources/input.js';
 const firstPartScores = new Map<string, number>([
   ['A X', 4], // rock/rock draw 1+3 
   ['A Y', 8], // rock/paper win 2+6
-  ['A Z', 3], // rock/scissors lost 3+0
-  ['B X', 1], // paper/rock lost 1+0
+  ['A Z', 3], // rock/scissors lose 3+0
+  ['B X', 1], // paper/rock lose 1+0
   ['B Y', 5], // paper/paper draw 2+3
   ['B Z', 9], // paper/scissors win 3+6
   ['C X', 7], // scissors/rock win 1+6
-  ['C Y', 2], // scissors/paper lost 2+0
+  ['C Y', 2], // scissors/paper lose 2+0
   ['C Z', 6], // scissors/scissors draw 3+3
 ]);
 
@@ -16,15 +16,15 @@ const firstPartScores = new Map<string, number>([
 // Y YOU MUST DRAW
 // Z YOU MUST WIN
 const secondPartScores = new Map<string, number>([
-  ['A X', 3], // rock/lose 
-  ['A Y', 4], // rock/draw 
-  ['A Z', 8], // rock/win 
-  ['B X', 1], // paper/lose 
-  ['B Y', 5], // paper/draw
-  ['B Z', 9], // paper/win
-  ['C X', 2], // scissors/lose 
-  ['C Y', 6], // scissors/draw 
-  ['C Z', 7], // scissors/win 
+  ['A X', 3], // opponent plays rock / you "must" lose 
+  ['A Y', 4], // opponent plays rock / you "must" draw 
+  ['A Z', 8], // opponent plays rock / you "must" win 
+  ['B X', 1], // opponent plays paper / you "must" lose 
+  ['B Y', 5], // opponent plays paper / you "must" draw
+  ['B Z', 9], // opponent plays paper / you "must" win
+  ['C X', 2], // opponent plays scissors / you "must" lose 
+  ['C Y', 6], // opponent plays scissors / you "must" draw 
+  ['C Z', 7], // opponent plays scissors / you "must" win 
 ]);
 
 const play = (games: string[], scores: Map<string, number>): number => games.map(
