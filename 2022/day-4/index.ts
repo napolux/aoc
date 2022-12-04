fccd04726e159f3dd7409fec1d703764fbf36d89
Napolux <napolux@gmail.com>
2022-12-04 15:34:14 +0100

    improving code

--- 2022/day-4/index.ts
+++ 2022/day-4/index.ts
@@ -3,29 +3,29 @@ import { getInput } from "../../utils/index.js";
 const START = 0;
 const END = 1;
 
-const isPartiallyOverlapping = (assignments: string[]): boolean => {
-  const firstElf = assignments[0].split('-').map(section => parseInt(section));
-  const secondElf = assignments[1].split('-').map(section => parseInt(section));
+const checkOverlap = (first: number[], second: number[], partialOverlap = false): boolean =>
+  (partialOverlap)
+    ? first[START] >= second[START] && first[START] <= second[END]
+    : first[START] <= second[START] && first[END] >= second[END];
 
-  return (
-    secondElf[START] >= firstElf[START] && secondElf[START] <= firstElf[END] ||
-    firstElf[START] >= secondElf[START] && firstElf[START] <= secondElf[END]
-  )
-}
+const partialOverlap = (assignments: number[][]): boolean =>
+  checkOverlap(assignments[0], assignments[1], true) || checkOverlap(assignments[1], assignments[0], true);
 
-const isCompletelyOverlapping = (assignments: string[]): boolean => {
-  const firstElf = assignments[0].split('-').map(section => parseInt(section));
-  const secondElf = assignments[1].split('-').map(section => parseInt(section));
-  return (
-    firstElf[START] <= secondElf[START] && firstElf[END] >= secondElf[END] ||
-    secondElf[START] <= firstElf[START] && secondElf[END] >= firstElf[END]
-  )
-}
+const completeOverlap = (assignments: number[][]): boolean =>
+  checkOverlap(assignments[0], assignments[1]) || checkOverlap(assignments[1], assignments[0]);
 
 // crunching input
 const input: string[][] = getInput(2022, 4).split('\n').map(assignments => assignments.split(','));
 
+const getElvesAssignments = (assignment: string[]) =>
+  assignment.map(a => a.split('-').map(Number));
+
 export const firstPart = (): number =>
-  input.filter(assignment => isCompletelyOverlapping(assignment)).length;
+  input.filter(assignment => completeOverlap(
+    getElvesAssignments(assignment)
+  )).length;
+
 export const secondPart = (): number =>
-  input.filter(assignment => isPartiallyOverlapping(assignment)).length;
+  input.filter(assignment => partialOverlap(
+    getElvesAssignments(assignment)
+  )).length;
