e06d19046b8360c023b310793e224929c60e7817
Napolux <napolux@gmail.com>
2022-12-06 12:02:51 +0100

    removing var assignment

--- 2022/day-6/index.ts
+++ 2022/day-6/index.ts
@@ -5,11 +5,9 @@ const MSG_INTERVAL = 14;
 
 const getContentStart = (data: string, interval = HEADER_INTERVAL): number => {
   for (let i = 0; i < data.length - interval; i++) {
-    const set = new Set<string>();
-    // inserting data in a set in order to spot duplicates easily
-    data.slice(i, i + interval).split('').forEach(char => set.add(char));
-    if (set.size === interval)
+    if (new Set(data.slice(i, i + interval).split('')).size === interval) {
       return i + interval;
+    }
   }
   return 0;
 }
