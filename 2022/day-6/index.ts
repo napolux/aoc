29a6a363c3857a2b0c5740d6bb4a8afeb56f9d72
Napolux <napolux@gmail.com>
2022-12-06 12:06:25 +0100

    comments

--- 2022/day-6/index.ts
+++ 2022/day-6/index.ts
@@ -4,7 +4,9 @@ const HEADER_INTERVAL = 4;
 const MSG_INTERVAL = 14;
 
 const getContentStart = (data: string, interval = HEADER_INTERVAL): number => {
+  // moving window on input
   for (let i = 0; i < data.length - interval; i++) {
+    // if our slice has duplicates, the Set size will be lower than the interval
     if (new Set(data.slice(i, i + interval).split('')).size === interval) {
       return i + interval;
     }
