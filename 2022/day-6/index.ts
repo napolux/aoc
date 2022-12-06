7f2b49eef3264463768902deccbc20d62a0c62cb
Napolux <napolux@gmail.com>
2022-12-06 09:40:35 +0100

    moving to a set

--- 2022/day-6/index.ts
+++ 2022/day-6/index.ts
@@ -3,18 +3,13 @@ import { getInput } from "../../utils/index.js";
 const HEADER_INTERVAL = 4;
 const MSG_INTERVAL = 14;
 
-const getStart = (data: string, interval = HEADER_INTERVAL): number => {
+const getContentStart = (data: string, interval = HEADER_INTERVAL): number => {
   for (let i = 0; i < data.length - interval; i++) {
-    const map = new Map<string, number>();
-    // inserting data in a map
-    data.slice(i, i + interval).split('').map(
-      char => map.has(char)
-        ? map.set(char, map.get(char) + 1)
-        : map.set(char, 1));
-
-    if (Array.from(map.values()).filter(value => value === 1).length === interval) {
+    const set = new Set<string>();
+    // inserting data in a set in order to spot duplicates easily
+    data.slice(i, i + interval).split('').forEach(char => set.add(char));
+    if (set.size === interval)
       return i + interval;
-    }
   }
   return 0;
 }
@@ -22,5 +17,5 @@ const getStart = (data: string, interval = HEADER_INTERVAL): number => {
 // crunching input
 const input: string = getInput(2022, 6);
 
-export const firstPart = (): number => getStart(input, HEADER_INTERVAL);
-export const secondPart = (): number => getStart(input, MSG_INTERVAL);
+export const firstPart = (): number => getContentStart(input, HEADER_INTERVAL);
+export const secondPart = (): number => getContentStart(input, MSG_INTERVAL);
