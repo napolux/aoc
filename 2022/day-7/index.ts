2898f0718fa7ca1459112b18142f55696aa61d50
Napolux <napolux@gmail.com>
2022-12-07 13:20:32 +0100

    some refactoring

--- 2022/day-7/index.ts
+++ 2022/day-7/index.ts
@@ -16,15 +16,6 @@ const TOTAL_DISK_SPACE = 70000000;
 const REQUIRED_DISK_SPACE = 30000000;
 const ROOT_FOLDER_ID = 1;
 
-// crunching input
-const input: string[] = getInput(2022, 7).split('\n');
-
-let id = 0;
-const getNodeId = () => {
-  id++;
-  return id;
-}
-
 enum TypeEnum {
   FILE,
   FOLDER,
@@ -39,11 +30,16 @@ type Node = {
 }
 
 const getFilesystem = (input: string[]): Node[] => {
-  const fs: Node[] = [{ id: getNodeId(), name: DIR_ROOT, parentId: -1, type: TypeEnum.FOLDER }];
-  let currentFolder = fs[0];
+  let currentNodeId = 1;
+  const fs: Node[] = [{ id: currentNodeId++, name: DIR_ROOT, parentId: null, type: TypeEnum.FOLDER }];
+  let currentDir = fs[0];
   input.forEach(line => {
     const cmd = line.split(' ');
     switch (cmd[0]) {
+      // listing a directory, adding it to the tree
+      case DIR:
+        fs.push({ id: currentNodeId++, name: cmd[1], parentId: currentDir.id, type: TypeEnum.FOLDER });
+        break;
       // launching a command
       case PROMPT:
         // we don't care about ls
@@ -51,23 +47,18 @@ const getFilesystem = (input: string[]): Node[] => {
         // changing directory
         if (cmd[1] === COMMANDS.CD) {
           if (cmd[2] === DIR_ROOT) {
-            currentFolder = fs[0];
+            currentDir = fs[0];
           } else if (cmd[2] === DIR_UP) {
-            if (currentFolder.name === DIR_ROOT) return;
-            currentFolder = find(fs, { id: currentFolder.parentId });
+            currentDir = (currentDir.name === DIR_ROOT) ? fs[0] : find(fs, { id: currentDir.parentId });
           } else {
             // a new directory
-            currentFolder = find(fs, { parentId: currentFolder.id, name: cmd[2] })
+            currentDir = find(fs, { parentId: currentDir.id, name: cmd[2] })
           }
         }
         break;
-      // listing a directory, adding it to the tree
-      case DIR:
-        fs.push({ id: getNodeId(), name: cmd[1], parentId: currentFolder.id, type: TypeEnum.FOLDER });
-        break;
       // listing a file
       default:
-        fs.push({ id: getNodeId(), name: cmd[1], parentId: currentFolder.id, type: TypeEnum.FILE, size: parseInt(cmd[0]) });
+        fs.push({ id: currentNodeId++, name: cmd[1], parentId: currentDir.id, type: TypeEnum.FILE, size: parseInt(cmd[0]) });
         break;
     }
   });
@@ -78,26 +69,18 @@ const getFolderSize = (fs: Node[], nodeId: number): number =>
   fs.filter((node) => node.parentId === nodeId)
     .reduce((a, b) => a + (b.type === TypeEnum.FILE ? b.size : getFolderSize(fs, b.id)), 0);
 
-export const firstPart = (): number => {
-  // get the filesystem
-  const fs = getFilesystem(input);
-  // get folder sizes
-  const folderSizes = fs
-    .filter((node) => node.type === TypeEnum.FOLDER)
-    .map((node) => getFolderSize(fs, node.id));
+// crunching input
+const input: string[] = getInput(2022, 7).split('\n');
+const fs = getFilesystem(input);
+const folderSizes = fs
+  .filter((node) => node.type === TypeEnum.FOLDER)
+  .map((node) => getFolderSize(fs, node.id));
+
+export const firstPart = (): number =>
   // return the sum of folder sizes that are less than MAX_FOLDER_SIZE
-  return folderSizes.filter((size) => size <= MAX_FOLDER_SIZE).reduce((a, b) => a + b, 0);
-};
+  folderSizes.filter((size) => size <= MAX_FOLDER_SIZE).reduce((a, b) => a + b, 0);
 
 export const secondPart = (): number => {
-  // reset id count...
-  id = 0;
-  const fs = getFilesystem(input);
-  // get folder sizes
-  const folderSizes = fs
-    .filter((node) => node.type === TypeEnum.FOLDER)
-    .map((node) => getFolderSize(fs, node.id));
-
   const requiredSpaceNeeded = REQUIRED_DISK_SPACE - (TOTAL_DISK_SPACE - getFolderSize(fs, ROOT_FOLDER_ID));
   // getting the first folder with space used >= space required
   return folderSizes.sort((a, b) => a - b).find((s) => s >= requiredSpaceNeeded);
