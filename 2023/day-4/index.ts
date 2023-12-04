29e07ef474e9b39012472609a5fe628e56fb25ec
Napolux <napolux@gmail.com>
2023-12-04 13:40:02 +0100

    day 4, part 2

--- 2023/day-4/index.ts
+++ 2023/day-4/index.ts
@@ -1,7 +1,7 @@
 import { getInput } from "../../utils/index.js";
 
 interface Card {
-  gameNum: number;
+  cardNum: number;
   winning: Array<number>;
   yours: Array<number>;
 }
@@ -11,20 +11,30 @@ const getWinnings = (cards: Array<Card>): number =>
     .map((card) => card.winning.filter((num) => card.yours.includes(num)))
     .reduce((acc, winnings) => acc + ((winnings.length > 0) ? Math.pow(2, winnings.length - 1) : 0), 0);
 
-// const getCards = (games: Array<Card>): number => {
-//   const stack = 
-// }
+const getCardsCount = (cards: Array<Card>): number => {
+  // starting cards
+  const cardsCount: number[] = new Array(cards.length).fill(1);
+  // for each card we increment the count of the following cards, according to the wins
+  cards.forEach((card, index) => {
+    const wins = card.winning.filter((num) => card.yours.includes(num)).length;
+    for (let i = 1; i <= wins; i++) {
+      cardsCount[index + i] += cardsCount[index];
+    }
+  });
+
+  return cardsCount.reduce((a, b) => a + b, 0);
+}
 
 const crunchInput = (): Array<Card> => {
   const lines = getInput(2023, 4).split('\n');
   return lines.map((line) => {
     const [num, card] = line.split(': ');
-    const gameNum = +num.match(/\d+/)[0];
+    const cardNum = +num.match(/\d+/)[0];
     const [w, y] = card.split(' | ');
     const winning = w.match(/\d+/g).map(n => +n);
     const yours = y.match(/\d+/g).map(n => +n);
     return {
-      gameNum,
+      cardNum,
       winning,
       yours
     }
@@ -35,4 +45,4 @@ const crunchInput = (): Array<Card> => {
 const cards: Array<Card> = crunchInput();
 
 export const firstPart = (): number => getWinnings(cards);
-export const secondPart = (): number => 0;// getCoordinates(secondInput);
+export const secondPart = (): number => getCardsCount(cards);
