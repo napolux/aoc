1cea3ad7a88bd94063fdaea7d87670d8b13589cd
Napolux <napolux@gmail.com>
2023-12-09 21:47:10 +0100

    day 8, part 1

---
+++ 2023/day-8/index.ts
@@ -0,0 +1,43 @@
+import { getInput } from "../../utils/index.js";
+
+interface Data {
+  L: string;
+  R: string;
+}
+
+const countSteps = (dir: string, map: Map<string, Data>): number => {
+  let steps = 0;
+  let found = false;
+  let current = 'AAA';
+  while (!found) {
+    const next = map.get(current)[dir[steps % dir.length]];
+    if (next === 'ZZZ') {
+      found = true;
+    } else {
+      current = next;
+      steps++;
+    }
+  }
+  return steps + 1;
+}
+
+const crunchInput = (): { dir: string, map: Map<string, Data> } => {
+  const map = new Map<string, Data>();
+  const input = getInput(2023, 8).split('\n');
+  input.forEach((v, index) => {
+    if (index >= 2) {
+      const matches = v.match(/[A-Z]{3}/g)
+      map.set(matches[0], { L: matches[1], R: matches[2] })
+    }
+  });
+
+  return {
+    dir: input[0],
+    map,
+  }
+}
+
+const { dir, map } = crunchInput();
+
+export const firstPart = (): number => countSteps(dir, map);
+export const secondPart = (): number => 0;
